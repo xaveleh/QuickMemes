b82a6e93dd863fea8e9305721224b5b8c67e4cd7
Xava <xavadecordova@gmail.com>
2019-09-13 17:07:09 -0400

    removed unnecessary import of lodash in memetext

--- src/memetext.js
+++ src/memetext.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { MemeContext } from './MemeContext';
-import _ from 'lodash';
 
 const MemeTextInput = styled.label`
     display: block;
