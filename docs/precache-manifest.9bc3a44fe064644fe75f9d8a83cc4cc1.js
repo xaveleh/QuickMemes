c0179d0422d74b9ae0c04cdd8696123ebda79540
Xava <xavadecordova@gmail.com>
2019-09-13 16:05:01 -0400

    created production build

---
+++ docs/precache-manifest.9bc3a44fe064644fe75f9d8a83cc4cc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0327cc6c04d0e214a53c094b792340ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f3d414161a6ac340a00",
+    "url": "/static/css/main.630209b6.chunk.css"
+  },
+  {
+    "revision": "b6f4e79ade0e3539c079",
+    "url": "/static/js/2.e4680f25.chunk.js"
+  },
+  {
+    "revision": "6f3d414161a6ac340a00",
+    "url": "/static/js/main.222cf1df.chunk.js"
+  },
+  {
+    "revision": "daaae7a2cc3f3c418ba4",
+    "url": "/static/js/runtime~main.d8f5f4c4.js"
+  }
+]);
