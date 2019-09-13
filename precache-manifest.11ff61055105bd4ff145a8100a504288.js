e7096c04d22deca2a2f70ff2b28df1c93c38e6e8
Xava <xavadecordova@gmail.com>
2019-09-13 18:12:23 -0400

    Updates

---
+++ precache-manifest.11ff61055105bd4ff145a8100a504288.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c65090b166bfd4b3d27c83957ec26ff",
+    "url": "/QuickMemes/index.html"
+  },
+  {
+    "revision": "96b616ede6037f8afdd0",
+    "url": "/QuickMemes/static/css/main.9b2da3ce.chunk.css"
+  },
+  {
+    "revision": "9e7cdede150f4ff53d26",
+    "url": "/QuickMemes/static/js/2.5f825843.chunk.js"
+  },
+  {
+    "revision": "96b616ede6037f8afdd0",
+    "url": "/QuickMemes/static/js/main.1fe62f6e.chunk.js"
+  },
+  {
+    "revision": "b7da2ca72ff2573a1ee7",
+    "url": "/QuickMemes/static/js/runtime~main.8a5609a2.js"
+  }
+]);
